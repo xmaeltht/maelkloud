778db97e5e396c4adb9d5bf560fd7a014f94623c
maelkloud <maelkloud@gmail.com>
2023-04-01 00:31:18 -0400

    Second commit

--- 0-ansible-aws/ansible_check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script is used to check the connection between ansible and the host
-
-ansible all -i ansible_inventory.ini -m ping
-
-ansible all -i ansible_inventory.ini -m command -a "uptime"
---
+++ 0-ansible-aws/terra.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script is used to deploy aws infrastructure that ansible will use to interact with.
+
+terraform init
+terraform apply -auto-approve
+
+# This is used to check the connection between ansible and the host
+
+ansible all -i ansible_inventory.ini -m ping
+
+ansible all -i ansible_inventory.ini -m command -a "uptime"
